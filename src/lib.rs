d964026d62398f2b12f9de5f4a12728de6d5d49d
mbs9 <66876095+MBS9@users.noreply.github.com>
2024-07-24 11:26:50 +0200

    fix: adjust algo to my needs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 use pyo3::{exceptions::PyValueError, prelude::*};
 
 /// Modified version of this: https://en.wikipedia.org/wiki/Longest_common_substring
+/// Returns index and substring of substrings of length at least min
+/// Does not return overlapping substrings
 #[pyfunction]
-fn common_substring(mut a: String, mut b: String, min: usize) -> PyResult<Vec<String>> {
-    // validate that min is at least 2
+fn common_substring(mut b: String, mut a: String, min: usize) -> PyResult<Vec<(usize, String)>> {
     a.push(char::MAX);
     b.push('\n');
     if min < 2 {
         return Err(PyValueError::new_err("min must be at least 2"));
     }
     let mut l: Vec<Vec<usize>> = vec![vec![0usize; b.len()]; a.len()];
-    let mut ret: Vec<String> = Vec::new();
+    let mut ret: Vec<(usize, String)> = Vec::new();
     for (i, c) in a.chars().enumerate() {
         for (j, d) in b.chars().enumerate() {
             if c == d {
@@ -27,7 +28,7 @@ fn common_substring(mut a: String, mut b: String, min: usize) -> PyResult<Vec<St
                 if l[i - 1][j - 1] >= min {
                     // We don't need the -1 because the range is exclusive on the right side
                     // We don't need the -1 in the main formula, because the array starts from 0, but string length starts from 1
-                    ret.push(a[i - l[i - 1][j - 1] + 0..(i)].to_string());
+                    ret.push((i-1, a[i - l[i - 1][j - 1] + 0..(i)].to_string()));
                 }
             }
         }
