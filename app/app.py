cd2fecb7a6d671ea49a3869dead7b1397e6b0af3
mbs9 <66876095+MBS9@users.noreply.github.com>
2024-07-29 17:53:45 +0200

    update code

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 from aiohttp import web
-from asyncio import get_running_loop
+import asyncio
 import jinja2
 import analysis
 
@@ -10,34 +10,24 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'),
                          autoescape=jinja2.select_autoescape(default=True))
 
 resp = env.get_template("resp.html")
+form = env.get_template("form.html")
 
 async def handle(request: web.Request):
-    text1 = request.query.get('text1', "text1")
-    text2 = request.query.get('text2', "text2")
-    minLen = int(request.query.get('minLen', '3'))
-    ratio = float(request.query.get('ratio', '1'))
-    loop = get_running_loop()
-    text = await loop.run_in_executor(pool, lambda: analysis.analyse_data()(text1, text2, minLen, ratio))
+    post = await request.post()
+    text1 = post["textA"].file.read().decode('utf-8')
+    text2 = post['textB'].file.read().decode('utf-8')
+    minLen = int(post['minLen'])
+    ratio = float(post['ratio'])
+    loop = asyncio.get_running_loop()
+    text = await loop.run_in_executor(pool, lambda: analysis.analyse_data(text1, text2, minLen, ratio))
     return web.Response(text=resp.render(response=text), content_type='text/html')
-
-async def wshandle(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    async for msg in ws:
-        if msg.type == web.WSMsgType.text:
-            await ws.send_str("Hello, {}".format(msg.data))
-        elif msg.type == web.WSMsgType.binary:
-            await ws.send_bytes(msg.data)
-        elif msg.type == web.WSMsgType.close:
-            break
-
-    return ws
 
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/echo', wshandle)
+app.add_routes([web.post('/compare', handle),
+                web.get('/', lambda req: web.Response(
+                    text=form.render(),
+                    content_type='text/html'))
                 ])
 
 if __name__ == '__main__':
--- rust/test.py
+++ rust/test.py
@@ -3,3 +3,14 @@ import compare_text
 print(compare_text.common_substring('kkagbkkhcd', 'agbrkkhcd', 2))
 print(compare_text.common_substring_levenshtein('kkagbkkhcdhguz', 
                                                   'agbrkkhcdzgzu', 2, 0.8))
+
+print(compare_text.common_substring_levenshtein('xhello world abcdefghijklmnopqrstuvwxyz', 
+                                                  'yhello world abcdefghijklmnopqrstuvwxyz', 3, 0.9))
+
+## Know issue: if there are two strings like '222222222222222' and '2222222222222222', it will output repeating looking substrings
+## This is because the algorithm identifies overlapping substrings based on index, not content.
+## Same with 'ab ab ab ab ab' and 'ab ab ab ab ab ab'. It will output 'ab' and 'ab' as common substrings, despite them being part of the larger common substring 'ab ab ab ab ab'.
+## This because technically, the index of the characters is different, so it is not considered overlapping.
+## It is not a bug, but it is not a perfect solution either.
+
+print(compare_text.common_substring('ab ab ab ab ab ab','ab ab ab ab ab ab', 3))
