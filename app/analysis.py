be8134b63fcb616bef9c8909bbcb456e284fee12
mbs9 <66876095+MBS9@users.noreply.github.com>
2024-07-29 22:22:32 +0200

    remove uneccessary computation: this will be done by the client

--- app/analysis.py
+++ app/analysis.py
@@ -1,6 +1,4 @@
 from compare_text import common_substring_levenshtein
 
 def analyse_data(text1: str, text2: str, minLen: int, ratio: float):
-    text1 = text1.replace('\n', '').replace('\r', '')
-    text2 = text2.replace('\n', '').replace('\r', '')
     return common_substring_levenshtein(text1, text2, minLen, ratio)
