b438b8c80e0236f76aef96c41c326d8ea71d90b6
mbs9 <66876095+MBS9@users.noreply.github.com>
2024-07-31 21:10:46 +0200

    add memory trace

--- app/analysis.py
+++ app/analysis.py
@@ -1,6 +1,7 @@
 from compare_text import common_substring_levenshtein
 import numpy as np
 import pandas as pd
+import tracemalloc
 
 def cosineSimilarity(strA: str, strB: str, base: pd.Series):
   a = pd.Series(list(strA)).value_counts()
@@ -15,8 +16,8 @@ def cosineSimilarity(strA: str, strB: str, base: pd.Series):
 
 
 def analyse_data(textA: str, textB: str, minLen: int, ratio: float):
+    tracemalloc.start()
     base = pd.Series(0, index=pd.Index(list(textA+textB)).unique())
-
     levenshteinDistances = pd.DataFrame(common_substring_levenshtein(textA, textB, minLen, ratio),
                                         columns=['startA', 'startB', 'ratio', 'substringA', 'substringB'])
     levenshteinDistances['endA'] = levenshteinDistances['startA'] + levenshteinDistances['substringA'].str.len()
@@ -26,9 +27,13 @@ def analyse_data(textA: str, textB: str, minLen: int, ratio: float):
         for j, elem2 in levenshteinDistances.iterrows():
             ## if elem['endA'] >= elem2['startA'] or elem['endB'] >= elem2['startB']: continue
             a = textA[elem['endA']: elem2['startA']]
-            b = textB[elem['endB']: elem['startB']]
+            b = textB[elem['endB']: elem2['startB']]
             if a == '' or b == '': continue
             result.append(
                 [a, b, elem.to_dict(),
                  elem2.to_dict(), cosineSimilarity(a, b, base)])
-    return pd.DataFrame(result, columns=['betweenInA', 'betweenInB', 'str1', 'str2', 'cosine']).to_dict('records')
+    ret =  pd.DataFrame(result, columns=['betweenInA', 'betweenInB', 'str1', 'str2', 'cosine']).to_dict('records')
+    print('MEMORY USAGE: CURRENT, PEAK')
+    print(tracemalloc.get_traced_memory())
+    print(tracemalloc.stop())
+    return ret
