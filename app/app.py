822a0ff8ac29166210d32b43bd13b315c060c50f
mbs9 <66876095+MBS9@users.noreply.github.com>
2024-09-20 19:44:26 +0200

    small eror improvement

--- app/app.py
+++ app/app.py
@@ -13,9 +13,9 @@ pool = ThreadPoolExecutor(9) ## I think we can serve 10 clients with 512MB of me
 async def errorMiddleware(request, handler):
     try:
         return await handler(request)
-    except Exception as e:
-        logging.exception(e)
-        return web.Response(text=f"Internal server error: {e}", status=500)
+    except web.HTTPException as e:
+        logging.error(e)
+        return web.Response(status=e.status, text='', content_type='text/plain')
 
 async def handle(request: web.Request):
     post = await request.post()
@@ -28,9 +28,11 @@ async def handle(request: web.Request):
     text = await loop.run_in_executor(pool, lambda: analysis.analyse_data(text1, text2, minLen, ratio, maxStrikes))
     return web.Response(text=text, content_type='text/json', headers={'Access-Control-Allow-Origin': '*'})
 
+async def root(request):
+    return web.Response(text="", content_type='text/plain', status=200)
 
 app = web.Application(middlewares=[errorMiddleware])
-app.add_routes([web.post('/compare', handle)])
+app.add_routes([web.post('/compare', handle), web.get('/', root)])
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
