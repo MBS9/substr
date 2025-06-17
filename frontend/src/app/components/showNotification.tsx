267fce4eaf08b6615bb3a67e6983b7529ec1ce22
mbs9 <66876095+MBS9@users.noreply.github.com>
2025-06-17 16:19:55 +0200

    customize icons

--- frontend/src/app/components/showNotification.tsx
+++ frontend/src/app/components/showNotification.tsx
@@ -1,9 +1,14 @@
 "use client";
-import { Alert, Snackbar, AlertProps } from '@mui/material';
+import { Alert, Snackbar, AlertProps, SnackbarCloseReason, SvgIconTypeMap } from '@mui/material';
 import Slide, { SlideProps } from '@mui/material/Slide';
+import CheckIcon from '@mui/icons-material/Check';
+import ErrorIcon from '@mui/icons-material/Error';
+import WarningIcon from '@mui/icons-material/Warning';
+import InfoIcon from '@mui/icons-material/Info';
 import React from 'react';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
-type Severity = AlertProps['severity'];
+type Severity = AlertProps['severity'] extends undefined | (infer U) ? U : never;
 
 type NotificationContextType = {
     message: string;
@@ -22,6 +27,14 @@ export function useNotification() {
     return context?.setMessage ?? (() => { throw new Error('ERROR! Notification context not available'); });
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+const ICONS: { [Key in Severity]: OverridableComponent<SvgIconTypeMap<{}, "svg">> } = {
+    'success': CheckIcon,
+    'info': InfoIcon,
+    'warning': WarningIcon,
+    'error': ErrorIcon,
+}
+
 export function ShowNotification({ children }: { children: React.ReactNode }) {
     const [snackbarOpen, setSnackbarOpen] = React.useState(false);
     const [message, setMessage] = React.useState<string>('');
@@ -32,6 +45,14 @@ export function ShowNotification({ children }: { children: React.ReactNode }) {
         setSnackbarOpen(true);
     }, [setMessage]);
 
+    const handleClose = React.useCallback((event?: React.SyntheticEvent<any> | Event, reason?: SnackbarCloseReason) => {
+        if (reason === 'clickaway')
+            return;
+        setSnackbarOpen(false);
+    }, []);
+
+    const Icon = React.useMemo(() => ICONS[severity], [severity]);
+
     return (
         <>
             <NotificationContext.Provider value={{ message: message, severity: severity, setMessage: setMessageCallback }}>
@@ -41,14 +62,14 @@ export function ShowNotification({ children }: { children: React.ReactNode }) {
                 autoHideDuration={10000}
                 open={snackbarOpen}
                 slots={{ transition: SlideTransition }}
-                onClose={() => setSnackbarOpen(false)}
-
+                onClose={handleClose}
             >
                 <Alert
                     severity={severity}
                     variant='filled'
+                    icon={<Icon fontSize='inherit' />}
                     sx={{ width: '100%' }}
-                    onClose={() => setSnackbarOpen(false)}
+                    onClose={handleClose}
                 >
                     {message}
                 </Alert>
