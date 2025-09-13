20d50f65c758eada72e4bb8bff8807ffca5523db
mbs9 <66876095+MBS9@users.noreply.github.com>
2025-09-13 03:03:55 +0200

    add guards

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,6 @@
 extern crate wasm_bindgen;
 use gloo_utils::format::JsValueSerdeExt;
-use std::panic::{self, PanicHookInfo};
+use std::{cmp::min, panic::{self, PanicHookInfo}};
 use wasm_bindgen::prelude::*;
 
 
@@ -119,14 +119,15 @@ pub fn process(
         }
         *cosine
     };
+    let substract_one_if_not_0 = |x: usize| if x > 0 { x - 1 } else { 0 };
     result.push(add_cosine_similarity_to_result(&mut utils::Result {
         a: utils::Substring {
             start: 0,
-            end: token_a[matches_a.first().unwrap().start_a].end,
+            end: token_a[substract_one_if_not_0(matches_a.first().unwrap().start_a)].end,
         },
         b: utils::Substring {
             start: 0,
-            end: token_b[matches_b.first().unwrap().start_b].end,
+            end: token_b[substract_one_if_not_0(matches_b.first().unwrap().start_b)].end,
         },
         similarity: 0.0,
         levenshteinMatch: false,
@@ -135,12 +136,12 @@ pub fn process(
         // Get the area between two matches in from tokens_a and b
         let mut cosine = utils::Result {
             a: utils::Substring {
-                start: token_a[tokens_a[0].end_a].start,
-                end: token_a[tokens_a[1].start_a].end,
+                start: token_a[min(tokens_a[0].end_a+1, token_a.len()-1)].start,
+                end: token_a[substract_one_if_not_0(tokens_a[1].start_a)].end,
             },
             b: utils::Substring {
-                start: token_b[tokens_b[0].end_b].start,
-                end: token_b[tokens_b[1].start_b].end,
+                start: token_b[min(tokens_b[0].end_b+1, token_b.len()-1)].start,
+                end: token_b[substract_one_if_not_0(tokens_b[1].start_b)].end,
             },
             similarity: 0.0,
             levenshteinMatch: false,
@@ -149,11 +150,11 @@ pub fn process(
     }
     result.push(add_cosine_similarity_to_result(&mut utils::Result {
         a: utils::Substring {
-            start: token_a[matches_a.last().unwrap().end_a].start,
+            start: token_a[min(matches_a.last().unwrap().end_a+1, token_a.len()-1)].start,
             end: file_a.len(),
         },
         b: utils::Substring {
-            start: token_b[matches_b.last().unwrap().end_b].start,
+            start: token_b[min(matches_b.last().unwrap().end_b+1, token_b.len()-1)].start,
             end: file_b.len(),
         },
         similarity: 0.0,
